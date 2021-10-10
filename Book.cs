38d2f88b75696000fa702a81499be617b2ed1d04
Johnson Dubula <jdubula@iqbusiness.net>
2021-10-11 01:06:55 +0200

    Add project files.

---
+++ Book.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FunctionApp35
+{
+   public class Book
+    {
+        public int BookID { get; set; }
+
+    }
+}
