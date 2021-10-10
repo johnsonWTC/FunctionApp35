bcc4843a6b6012f3cf44cc506225c3df53a719b1
Johnson Dubula <jdubula@iqbusiness.net>
2021-10-11 01:16:50 +0200

    Functions

---
+++ BookContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FunctionApp35
+{
+    class BookContext : DbContext
+    {
+        public BookContext(DbContextOptions<BookContext> options) : base(options)
+        {
+        }
+        public DbSet<Book> Book { get; set; }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+[assembly: FunctionsStartup(typeof(FunctionApp35.Startup))]
+namespace FunctionApp35
+{
+
+    public class Startup : FunctionsStartup
+    {
+        private IConfiguration Configuration;
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            builder.Services.AddDbContext<BookContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+        }
+    }
+
+}
